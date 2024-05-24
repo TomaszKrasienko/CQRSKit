9d2dad1fda674387541f512169b477e7524ba67e
Tomasz Krasieńko <94125393+TomaszKrasienko@users.noreply.github.com>
2024-05-24 12:41:31 +0200

    Aded configuration. Untrack files contained in the .gitignore file

---
+++ CQRSKit/CQRSKit/Commands/Configuration/Extensions.cs
@@ -0,0 +1,23 @@
+using System.Reflection;
+using CQRSKit.Commands.Abstractions;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CQRSKit.Commands.Configuration;
+
+internal static class Extensions
+{
+    internal static IServiceCollection AddCommands(this IServiceCollection services)
+    {
+        IEnumerable<Assembly> assemblies = AppDomain.CurrentDomain.GetAssemblies();
+        return services.AddCommands(assemblies);
+    }
+
+    private static IServiceCollection AddCommands(this IServiceCollection services, IEnumerable<Assembly> assemblies)
+    {
+        services.Scan(s => s.FromAssemblies(assemblies)
+            .AddClasses(c => c.AssignableTo(typeof(ICommandHandler<>)))
+            .AsImplementedInterfaces()
+            .WithScopedLifetime());
+        return services;
+    }
+}
